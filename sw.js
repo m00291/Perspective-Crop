eb4d8b0805546acb393db9a352de6eac67903a05
m00291 <62349178+m00291@users.noreply.github.com>
2026-08-04 14:41:07 +0800

    v.2.3.41

---
+++ sw.js
@@ -0,0 +1,34 @@
+const APP_PREFIX = 'perspective-crop-';
+const CACHE_NAME = `${APP_PREFIX}v2.3.41`;
+const ASSETS = [
+  './index.html',
+  './crop.svg',
+  './style.css',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key.startsWith(APP_PREFIX) && key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
